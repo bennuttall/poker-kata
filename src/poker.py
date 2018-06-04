a830c633b506b258a0448522688988fd58f354cc
Ben Nuttall <ben@bennuttall.com>
2018-06-04 22:11:10 +0100

    Improve starter code

--- src/poker.py
+++ src/poker.py
@@ -1,25 +1,30 @@
 from itertools import product
 from random import shuffle
 
-VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
-SUITS = ('H', 'D', 'S', 'C')
+VALUES = '23456789TJQKA'
+SUITS = 'HDSC'
 
-HAND_TYPES = (
-        'High Card',
-        'Pair',
-        'Two Pairs',
-        'Three of a Kind',
-        'Straight',
-        'Flush',
-        'Full House',
-        'Four of a Kind',
-        'Straight Flush'
+HAND_RANKS = (
+    'High Card',
+    'Pair',
+    'Two Pairs',
+    'Three of a Kind',
+    'Straight',
+    'Flush',
+    'Full House',
+    'Four of a Kind',
+    'Straight Flush'
 )
 
 class Card:
-    def __init__(self, value, suit):
-        value = str(value)
-        suit = str(suit)
+    """
+    Represents a single playing card. Initialise with a two-character string in
+    the form <value><suit> e.g. AS for the ace of spades:
+
+    >>> card = Card('AS')
+    """
+    def __init__(self, value_suit):
+        value, suit = value_suit
         if value not in VALUES:
             raise ValueError
         self.value = value
@@ -28,7 +33,10 @@ class Card:
         self.suit = suit
 
     def __repr__(self):
-        return '<Card object {}{}>'.format(self.value, self.suit)
+        return '<Card object {}>'.format(self)
+
+    def __str__(self):
+        return '{}{}'.format(self.value, self.suit)
 
     def __eq__(self, other):
         return self.value == other.value
@@ -45,19 +53,29 @@ class Card:
 
 
 class PokerHand:
+    """
+    Represents a hand of five cards. Initialise with five 2-character strings
+    each separated by a space, e.g:
+
+    >>> hand = PokerHand('3D JC 8S 4H 2C')
+
+    Evaluate the hand rank by accessing the rank property:
+
+    >>> hand.rank
+    'High Card'
+    """
     def __init__(self, cards):
-        if any(not isinstance(card, Card) for card in cards):
-            raise ValueError
-        if len(cards) != 5:
-            raise ValueError
+        cards = cards.split(' ')
+        self.cards = [Card(sv) for sv in cards]
         card_set = {repr(card) for card in cards}
         duplicate_cards = len(card_set) < len(cards)
         if duplicate_cards:
             raise ValueError
-        self.cards = sorted(cards)
+        self.cards = sorted(self.cards)
 
     def __repr__(self):
-        return '<PokerHand object {}>'.format(', '.join(str(card) for card in self))
+        card_strings = (str(card) for card in self)
+        return '<PokerHand object ({})>'.format(', '.join(card_strings))
 
     def __iter__(self):
         return iter(self.cards)
@@ -71,10 +89,6 @@ class PokerHand:
     def __eq__(self, other):
         return self.cards[-1] == other.cards[-1]
 
-def create_deck():
-    cards = [Card(value, suit) for value, suit in product(VALUES, SUITS)]
-    shuffle(cards)
-    return cards
-
-def deal_hand(n, deck):
-    return [deck.pop() for i in range(n)]
+    @property
+    def rank(self):
+        return 'High Card'
--- src/tests.py
+++ src/tests.py
@@ -1,48 +1,5 @@
-from poker import Card, PokerHand, create_deck, deal_hand
+from poker import Card, PokerHand
 
-# some useful card suit/value constants
-T = 'T'
-J = 'J'
-Q = 'Q'
-K = 'K'
-A = 'A'
-H = 'H'
-D = 'D'
-S = 'S'
-C = 'C'
-# you can use integers for the others (but not 10 - use T)
-
-# test single card
-card_1 = Card(A, S)
-card_2 = Card(A, D)
-assert card_1 == card_2
-card_3 = Card(K, S)
-assert card_1 != card_3
-assert card_1 > card_3
-assert not card_1 < card_3
-
-# test comparing two poker hands
-cards = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
-hand_1 = PokerHand(cards)
-cards = [Card(v, s) for v, s in [(A, C), (3, D), (5, C), (8,  H), (T, S)]]
-hand_2 = PokerHand(cards)
-assert hand_1 == hand_2
-assert hand_2 == hand_1
-
-cards = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
-hand_1 = PokerHand(cards)
-cards = [Card(v, s) for v, s in [(Q, S), (3, D), (5, C), (8,  H), (T, S)]]
-hand_2 = PokerHand(cards)
-assert hand_1 > hand_2
-assert hand_2 < hand_1
-
-# test creating a deck
-deck = create_deck()
-assert len(deck) == 52
-assert all(isinstance(card, Card) for card in deck)
-
-# test creating a hand from the deck
-cards = deal_hand(5, deck)
-hand_1 = PokerHand(cards)
-cards = deal_hand(5, deck)
-hand_2 = PokerHand(cards)
+# test high card rank
+hand_1 = PokerHand('3D JC 8S 4H 2C')
+assert hand_1.rank == 'High Card', hand_1.rank
