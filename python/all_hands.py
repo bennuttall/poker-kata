4ca1bd93264851f060ee3f5974cab5d1ef5ad1bc
Ben Nuttall <ben@bennuttall.com>
2018-06-07 23:08:40 +0100

    Add script to generate dict of all hands combos, and add gzipped json file of the output

---
+++ python/all_hands.py
@@ -0,0 +1,17 @@
+from poker import PokerHand, VALUES, SUITS
+from itertools import product, combinations
+import json
+
+deck = ['{}{}'.format(v, s) for v, s in product(VALUES, SUITS)]
+
+def sort_cards_string(card_string):
+    cards = card_string.split(' ')
+    return ' '.join(sorted(cards))
+
+hand_combinations = {
+    sort_cards_string(' '.join(c)): PokerHand(' '.join(c)).rank
+    for c in combinations(deck, 5)
+}
+
+with open('ranks.json', 'w') as f:
+    json.dump(hand_combinations, f)
